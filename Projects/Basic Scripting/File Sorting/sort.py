189492153575e829aa9f424e69dad565e8e4d1f8
anthony.dev <anthonylumantao.dev@gmail.com>
2026-06-14 17:10:21 +0800

    Script for sorting files in /Downloads

--- Projects/Basic Scripting/File Sorting/sort.py
+++ Projects/Basic Scripting/File Sorting/sort.py
@@ -5,7 +5,15 @@ def main():
     path = Path("/Users/anthonylumantao/Downloads")
     existing = {item.name for item in path.iterdir() if item.is_dir()}
 
-    directories = ["Documents", "Images", "Videos", "Activities", "Programs", "audio", "Activities"]
+    directories = [
+        "Documents",
+        "Images",
+        "Videos",
+        "Activities",
+        "Programs",
+        "audio",
+        "Activities",
+    ]
 
     for d in directories:
         if d not in existing:
@@ -13,21 +21,27 @@ def main():
 
     for item in path.iterdir():
         if item.is_file():
-            if item.name.endswith(
-                (".docx" , ".pdf", ".xlsx", ".pptx", ".csv", ".gz")):
-                    item.move(Path("/Users/anthonylumantao/Downloads/Documents") / item.name)
+            if item.name.endswith((".docx", ".pdf", ".xlsx", ".pptx", ".csv", ".gz")):
+                item.move(
+                    Path("/Users/anthonylumantao/Downloads/Documents") / item.name
+                )
             elif item.name.endswith((".py", ".java", ".ino")):
                 item.move(Path("/Users/anthonylumantao/Downloads/Programs") / item.name)
             elif item.name.endswith((".wav", ".mp3", ".m4a")):
                 item.move(Path("/Users/anthonylumantao/Downloads/audio") / item.name)
-            elif item.name.startswith(("Screenshots")) or item.name.endswith((".gif", ".png", ".jpg", ".jpeg")):
+            elif item.name.startswith(("Screenshots")) or item.name.endswith(
+                (".gif", ".png", ".jpg", ".jpeg")
+            ):
                 item.move(Path("/Users/anthonylumantao/Downloads/Images") / item.name)
-            elif item.name.endswith((".drawio", ".aep", ".pkt", ".zip", "md", ".piskel")):
-                item.move(Path("/Users/anthonylumantao/Downloads/Activities") / item.name)
+            elif item.name.endswith(
+                (".drawio", ".aep", ".pkt", ".zip", "md", ".piskel")
+            ):
+                item.move(
+                    Path("/Users/anthonylumantao/Downloads/Activities") / item.name
+                )
             else:
                 item.unlink()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
